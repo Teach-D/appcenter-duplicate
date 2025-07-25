9122c7d277b5a75ecef24551b385bde83dcf5fac
Jimin15 <sujean07015@gmail.com>
2025-07-26 08:19:15 +0900

    fix:룸메이트 매칭 로직 오류 수정 및 N+1 문제 해결 #18

--- src/main/java/com/example/appcenter_project/service/roommate/RoommateMatchingService.java
+++ src/main/java/com/example/appcenter_project/service/roommate/RoommateMatchingService.java
@@ -31,7 +31,7 @@ public class RoommateMatchingService {
         User receiver = userRepository.findByStudentNumber(receiverStudentNumber)
                 .orElseThrow(() -> new CustomException(ErrorCode.ROOMMATE_USER_NOT_FOUND));
 
-        if (receiver.getRoommateBoard() != null) {
+        if (myRoommateRepository.findByUserId(receiver.getId()).isPresent()) {
             throw new CustomException(ErrorCode.ROOMMATE_ALREADY_MATCHED);
         }
 
@@ -56,6 +56,7 @@ public class RoommateMatchingService {
     }
 
     // 매칭 수락
+    @Transactional
     public void acceptMatching(Long matchingId, Long userId) {
 
         User user = userRepository.findById(userId)
@@ -70,14 +71,13 @@ public class RoommateMatchingService {
             throw new CustomException(ErrorCode.ROOMMATE_MATCHING_NOT_FOR_USER);
         }
 
-
         // 이미 완료된 요청인지 확인
         if (matching.getStatus() != MatchingStatus.REQUEST) {
             throw new CustomException(ErrorCode.ROOMMATE_MATCHING_ALREADY_COMPLETED);
         }
 
         // 이미 매칭 완료된 사람인지 확인
-        if (user.getRoommateBoard() != null) {
+        if (myRoommateRepository.findByUserId(user.getId()).isPresent()) {
             throw new CustomException(ErrorCode.ROOMMATE_ALREADY_MATCHED);
         }
 
@@ -102,6 +102,7 @@ public class RoommateMatchingService {
     }
 
     // 매칭 거절
+    @Transactional
     public void rejectMatching(Long matchingId) {
         RoommateMatching matching = roommateMatchingRepository.findById(matchingId)
                 .orElseThrow(() -> new CustomException(ErrorCode.ROOMMATE_MATCHING_NOT_FOUND));
