8d9d3c6f8597c6a57b0cebc263088c0529884b6b
Teach-D <wkadht0619@gmail.com>
2025-08-05 15:13:58 +0900

    AttachedFileRepository integer -> Long

--- src/main/java/com/example/appcenter_project/repository/announcement/AttachedFileRepository.java
+++ src/main/java/com/example/appcenter_project/repository/announcement/AttachedFileRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface AttachedFileRepository extends JpaRepository<AttachedFile,Integer> {
+public interface AttachedFileRepository extends JpaRepository<AttachedFile, Long> {
     List<AttachedFile> findByAnnouncement(Announcement announcement);
     void deleteByFilePath(String filePath);
 
