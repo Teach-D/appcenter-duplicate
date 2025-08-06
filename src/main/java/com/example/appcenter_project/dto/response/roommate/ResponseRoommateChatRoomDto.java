a6e97ab589744ede5c4a0e0efb8a9e57db6ebbdb
Jimin15 <sujean07015@gmail.com>
2025-08-06 19:30:08 +0900

    fix: 채팅방 목록 마지막 메시지 시간 KST 기준으로 표기 #63

--- src/main/java/com/example/appcenter_project/dto/response/roommate/ResponseRoommateChatRoomDto.java
+++ src/main/java/com/example/appcenter_project/dto/response/roommate/ResponseRoommateChatRoomDto.java
@@ -1,5 +1,6 @@
 package com.example.appcenter_project.dto.response.roommate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -11,6 +12,7 @@ public class ResponseRoommateChatRoomDto {
     private Long chatRoomId;
     private String opponentNickname;
     private String lastMessage;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime lastMessageTime;
     private Long partnerId;
     private String partnerName;
